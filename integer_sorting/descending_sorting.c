87df485ff01433c73fe05c5c4c34d982868274f3
Chibuike Mba <chibuike.code@gmail.com>
2022-12-01 15:43:38 +0100

    Create descending_sorting.c
    This commit will use a descending sorting algorithm to arrange any input
    of three integers in a descending manner. Conditional if-else and
    if-else-if ladder statements were used so as to cover all possible
    permutation and combination of the integer inputs.

---
+++ integer_sorting/descending_sorting.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(void) {
+        int a, b, c;
+        printf("Please, enter three integers separated by commas: ");
+        scanf("%d, %d, %d", &a, &b, &c);
+
+        if (a >= 0 && b >= 0 && c >= 0 ) {
+                if (a > b && b > c) {
+                        printf("The given integers in descending order are: %d, %d, %d\n", a, b, c);
+                } else if (a > c && c > b) {
+                        printf("The given integers in descending order are: %d, %d, %d\n", a, c, b);
+                } else if (b > a && a > c) {
+                        printf("The given integers in descending order are: %d, %d, %d\n", b, a, c);
+                } else if (b > c && c > a) {
+                        printf("The given integers in descending order are: %d, %d, %d\n", b, c, a);
+                } else if (c > a && a > b) {
+                        printf("The given integers in descending order are: %d, %d, %d\n", c, a, b);
+                } else if (c > b && b > a) {
+                        printf("The given integers in descending order are: %d, %d, %d\n", c, b, a);
+                } else {
+                        printf("Please, enter three different integers greater than or equal to zero and separated by commas.\n");
+                }
+        } else {
+                 printf("Please, enter three integers greater than or equal to zero and separated by commas.\n");
+        }
+        return 0;
+}
